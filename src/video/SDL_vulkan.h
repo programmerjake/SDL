5e59e9a9425a04fb30f8794617555ff17dd9895a
Jacob Lifshay <programmerjake@gmail.com>
2017-02-21 06:55:30 +0000

    add basic framework for vulkan support

---
+++ src/video/SDL_vulkan.h
@@ -0,0 +1,43 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+#ifndef _SDL_vulkan_h
+#define _SDL_vulkan_h
+
+#include "SDL_vulkan_internal.h"
+
+#if SDL_VULKAN_SUPPORTED
+
+extern const char *SDL_Vulkan_GetResultString(VkResult result);
+
+extern VkExtensionProperties *SDL_Vulkan_CreateInstanceExtensionsList(
+    PFN_vkEnumerateInstanceExtensionProperties vkEnumerateInstanceExtensionProperties,
+    Uint32 *extensionCount); /* free returned list with SDL_free */
+
+/* implements functionality of SDL_Vulkan_GetInstanceExtensions for a list of names passed in
+ * nameCount and names */
+extern SDL_bool SDL_Vulkan_GetInstanceExtensions_Helper(unsigned *userCount,
+                                                        const char **userNames,
+                                                        unsigned nameCount,
+                                                        const char *const *names);
+
+#endif
+
+#endif
