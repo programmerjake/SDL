ba56d67100e9b2cf7b5bb61687b44ec31c1362a2
キャロウ　マーク <github@callow.im>
2017-06-08 18:25:39 -0700

    Fix for when VULKAN_SDK is set in cmake not environment & add Android check.

--- cmake/sdlchecks.cmake
+++ cmake/sdlchecks.cmake
@@ -1119,3 +1119,23 @@ macro(CheckRPI)
     endif(SDL_VIDEO AND HAVE_VIDEO_RPI)
   endif(VIDEO_RPI)
 endmacro(CheckRPI)
+
+macro(CheckVulkanHeaders)
+  if(VIDEO_VULKAN)
+    # ${VULKAN_SDK} could be unset during the first configure run with
+    # cmake-gui resulting in vulkan.h not being found. If it's been
+    # subsequently changed, unset is necessary to ensure check is run again.
+    unset(HAVE_VULKAN_H CACHE)
+    # Prefer ${VULKAN_SDK} header
+    set(CMAKE_REQUIRED_INCLUDES "${VULKAN_SDK}/include")
+    check_include_file("vulkan/vulkan.h" HAVE_VULKAN_H)
+    if(HAVE_VULKAN_H)
+      list(APPEND EXTRA_CFLAGS "-I${VULKAN_SDK}/include")
+    else()
+      # Check system includes.
+      unset(HAVE_VULKAN_H CACHE)
+      check_include_file("vulkan/vulkan.h" HAVE_VULKAN_H)
+    endif()
+  endif()
+endmacro(CheckVulkanHeaders)
+
