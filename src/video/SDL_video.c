bdaf437e3e45739360240b944a18475e4aa630c9
msc- <github@callow.im>
2017-08-11 18:27:39 -0700

    Remove dead code.

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -4059,13 +4059,6 @@ void SDL_Vulkan_GetDrawableSize(SDL_Window * window, int *w, int *h)
     } else {
         SDL_GetWindowSize(window, w, h);
     }
-#if 0
-#if SDL_VIDEO_DRIVER_UIKIT
-    UIKit_Mtl_GetDrawableSize(window, w, h);
-#elif SDL_VIDEO_DRIVER_COCOA
-    Cocoa_Mtl_GetDrawableSize(window, w, h);
-#endif
-#endif
 }
 
 /* vi: set ts=4 sw=4 expandtab: */
